26a234f62e138d9d5e1653f07c184e45e2608909
Ege-Okyay <83663983+Ege-Okyay@users.noreply.github.com>
2023-12-13 14:46:12 +0300

    Config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
-import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
-// https://astro.build/config
 export default defineConfig({
-  // site: "https://gianmarco.xyz/",
   integrations: [
     solidJs(),
     UnoCSS({ injectReset: true }),
